065de3f0bde4f0344dc7792b788859f871b8da1a
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-04 19:55:08 +0300

    test: plot draw() test, tidy-up test structure

--- benchmark/performance-runner/test/index.spec.js
+++ benchmark/performance-runner/test/index.spec.js
@@ -1,18 +1,6 @@
 // Run:
 // NODE_ENV=test node --test benchmark/performance-runner/test/index.spec.js
 
-import './runner/to-histograms/marks.spec.js'
-import './runner/to-histograms/to-histograms.spec.js'
-
-import './runner/to-timeline/marks.spec.js'
-import './runner/to-timeline/to-timeline.spec.js'
-
-import './runner/to-plots/to-plots.spec.js'
-
-import './runner/run/run.spec.js'
-import './runner/run/runner.spec.js'
-
-import './plot/update.spec.js'
-
-import './task/task.spec.js'
-import './task/run.spec.js'
+import './runner/index.spec.js'
+import './plot/index.spec.js'
+import './task/index.spec.js'
---
+++ benchmark/performance-runner/test/runner/index.spec.js
@@ -0,0 +1,10 @@
+import './to-histograms/marks.spec.js'
+import './to-histograms/to-histograms.spec.js'
+
+import './to-timeline/marks.spec.js'
+import './to-timeline/to-timeline.spec.js'
+
+import './to-plots/to-plots.spec.js'
+
+import './run/run.spec.js'
+import './run/runner.spec.js'
