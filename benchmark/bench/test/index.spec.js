ebada821bac4b405ca03d3c301aeb4f29d28952c
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-05 07:16:52 +0300

    fix: fix some paths

--- benchmark/bench/test/index.spec.js
+++ benchmark/bench/test/index.spec.js
@@ -1,5 +1,5 @@
 // Run:
-// NODE_ENV=test node --test benchmark/performance-runner/test/index.spec.js
+// NODE_ENV=test node --test benchmark/bench/test/index.spec.js
 
 import './runner/index.spec.js'
 import './plot/index.spec.js'
