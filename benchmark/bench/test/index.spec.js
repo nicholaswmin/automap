4e09ce6c1c26fc2226265bdc32ec12bc2594a291
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-06 04:26:11 +0300

    chore: more merge conflicts fixes

--- benchmark/bench/test/index.spec.js
+++ benchmark/bench/test/index.spec.js
@@ -1,10 +1,3 @@
-// Run:
-<<<<<<< HEAD
-// NODE_ENV=test node --test benchmark/bench/test/index.spec.js
-=======
-// NODE_ENV=test node --test benchmark/performance-runner/test/index.spec.js
->>>>>>> b8533fffd0b690a58411de0c79c8be30afb476ff
-
 import './runner/index.spec.js'
 import './plot/index.spec.js'
 import './task/index.spec.js'
