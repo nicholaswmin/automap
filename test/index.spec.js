5341ce6cd6f715273a8eb9f8beb47a9f0f68fad5
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-10 00:19:09 +0300

    test: fixed npm test code paths

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 // unit tests
-import '.unit/test/index.spec.js'
+import './unit/index.spec.js'
 
 // integration tests
-import '.unit/integration/index.spec.js'
+import './integration/index.spec.js'
 
 // test model
 import './model/test/index.spec.js'
