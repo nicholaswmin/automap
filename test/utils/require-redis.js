7768f25a8c30a9f00c93545e77d67b91d9436f15
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-10 21:25:59 +0300

    tests: improve redis not found message

--- test/utils/require-redis.js
+++ test/utils/require-redis.js
@@ -1,11 +1,13 @@
 import { styleText as c } from 'node:util'
 import ioredis from 'ioredis'
 
-const redis = new ioredis()
+const redis = new ioredis({ maxRetriesPerRequest: 0 })
 
 redis.on('connect', () => redis.disconnect())
-redis.on('error', () => {
-  console.info(c('cyan', `These tests require a locally running Redis server`))
-  console.error(c('red', `Error: cannot find a Redis server running at :6379`))
+redis.on('error', err => {
+  console.warn(c('yellow', `These tests require a Redis server running @:6379`))
+  console.error(c('red', `Redis ${err.toString()}`))
+
+  redis.disconnect()
   process.exit(1)
 })
