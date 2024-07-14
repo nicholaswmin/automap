a7e8cd0e6ce94743fe8ec3ea3c56d66489ebbbce
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-14 14:35:50 +0300

    test: remove a dep

--- .github/example/test/index.spec.js
+++ .github/example/test/index.spec.js
@@ -1,18 +1,15 @@
 import assert from 'node:assert'
 import { test } from 'node:test'
 
-import capcon from 'capture-console'
-
 test('Runnable example', async t => {
-  const logs = []
+  const logs = [], originalLogFn = console.log
 
-  await t.before(() => capcon.startCapture(process.stdout, stdout => {
-    // eslint-disable-next-line no-control-regex
-    const stripAnsi = str => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
-    logs.push(stripAnsi(stdout.toString()))
-  }))
+  await t.before(() =>
+    console.log = (...args) =>
+      logs.push(args.join(' ')))
 
-  await t.after(() => capcon.stopCapture(process.stdout))
+  await t.after(() => console.log = originalLogFn)
+
   await t.beforeEach(() => logs.splice(0, logs.length))
 
   await t.test('runs without errors', async () => {
@@ -24,23 +21,23 @@ test('Runnable example', async t => {
     await t.beforeEach(() =>
       import(`../index.js?bust_cache=${Date.now()}`))
 
-    await t.test('logs a save() success log', () => {
+    await t.test('a save() success log', () => {
       assert.ok(logs.some(log => log.includes('Building saved')))
     })
 
-    await t.test('logs a fetch() success log', () => {
+    await t.test('a fetch() success log', () => {
       assert.ok(logs.some(log => log.includes('Building fetched')))
     })
 
-    await t.test('logs a LazyList success log', () => {
+    await t.test('a LazyList success log', () => {
       assert.ok(logs.some(log => log.includes('Building has 2 flats')))
     })
 
-    await t.test('logs a Flat method called log', () => {
+    await t.test('a Flat method called log', () => {
       assert.ok(logs.some(log => log.includes('🔔 at flat 101')))
     })
 
-    await t.test('logs an AppendList message', () => {
+    await t.test('an AppendList log', () => {
       assert.ok(logs.some(log => log.includes('Flat 101 has 50 mails')))
     })
   })
