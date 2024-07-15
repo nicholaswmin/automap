f36ddbeea90b77d1914557b4c84c2001660c5805
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-15 23:55:27 +0300

    test: test example as part of integration tests instead

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,2 @@
 // unit tests
 import './unit/index.spec.js'
-
-// runnable example
-import '../.github/example/test/index.spec.js'
--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1,3 +1,6 @@
 import './list/index.spec.js'
 import './lazy-list/index.spec.js'
 import './append-list/index.spec.js'
+
+// runnable example
+import '../../.github/example/test/index.spec.js'
