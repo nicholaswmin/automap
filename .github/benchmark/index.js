21ffd50c9c4583a117cea26fbc942a9c8ab95436
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-25 10:37:02 +0300

    test: more cleanups

--- .github/benchmark/index.js
+++ .github/benchmark/index.js
@@ -1,6 +1,8 @@
 import cluster from 'node:cluster'
 
-import { constants, primary, worker } from './process/index.js'
+import constants from './src/constants/constants.js'
+import primary from './src/primary.js'
+import worker from './src/worker.js'
 
 cluster.isPrimary
   ? primary(cluster, await constants(cluster))
--- .github/benchmark/process/index.js
+++
@@ -1,5 +0,0 @@
-import constants from './constants/constants.js'
-import primary from './primary.js'
-import worker from './worker.js'
-
-export { constants, primary, worker }
