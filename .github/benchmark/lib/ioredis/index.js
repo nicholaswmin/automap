5af52a2d9077fea8f4548ca7fb96b9b11797a899
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-29 02:54:06 +0300

    docs: tweaks

--- .github/benchmark/lib/ioredis/index.js
+++ .github/benchmark/lib/ioredis/index.js
@@ -1,10 +1,10 @@
 import ioredis from 'ioredis'
 
 export default () => {
-  return new ioredis(process.env.REDIS_URL, {
+  return new ioredis('rediss://:p19b2193e1eacb89c8aa74af26aa51d67fa5d5e369138a9528a12d4b2c06614ca@ec2-52-214-237-224.eu-west-1.compute.amazonaws.com:31319', {
     keyPrefix: 'test:',
-    tls: process.env.REDIS_URL?.includes('rediss') ? {
+    tls: {
       rejectUnauthorized: false
-    } : undefined
+    }
   })
 }
