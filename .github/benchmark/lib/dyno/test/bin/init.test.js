74f290cc761f975e6f4ea768992d6b3a3c89718f
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-04 17:41:13 +0300

    test: minor tweaks

--- .github/benchmark/lib/dyno/test/bin/init.test.js
+++ .github/benchmark/lib/dyno/test/bin/init.test.js
@@ -2,5 +2,4 @@ import test from 'node:test'
 
 test('package.json: bin:init', async t => {
   t.todo('@todo: creates sample benchmark files')
-  t.todo('@todo: runs the sample benchmark')
 })
--- .github/benchmark/lib/dyno/test/index.test.js
+++ .github/benchmark/lib/dyno/test/index.test.js
@@ -1,1 +1,1 @@
-console.log('@TODO')
+import './bin/index.test.js'
