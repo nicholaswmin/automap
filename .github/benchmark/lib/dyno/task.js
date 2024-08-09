e22c00e45c10aeac16339a194e6873989b6ebdfe
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-09 13:08:21 +0300

    test: tweaks

--- .github/benchmark/lib/dyno/task.js
+++
@@ -1,8 +0,0 @@
-import { run } from './index.js'
-
-run(async function task(parameters) {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-  const timed_sleep = performance.timerify(sleep)
-  
-  await timed_sleep(Math.round(Math.random() * parameters.RAND_FACTOR))
-})
